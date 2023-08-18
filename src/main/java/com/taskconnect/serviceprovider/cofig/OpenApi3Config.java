95a9dff4e25fe1aafc4191a2908f85bcc495ce61
Daniel Alves <daniel.vdg@hotmail.com>
2023-08-18 12:16:25 -0300

    changing documentaion to OpenAPI3

---
+++ src/main/java/com/taskconnect/serviceprovider/cofig/OpenApi3Config.java
@@ -0,0 +1,16 @@
+package com.taskconnect.serviceprovider.cofig;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(
+        title = "provedor de Serviços",
+        description = "Serviços encontrar profissionais",
+        version = "v1"
+))
+public class OpenApi3Config {
+
+}
+
